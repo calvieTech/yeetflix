c12dac2b3b1b385bf4b767cc9eb61dbc3eaffb87
calvieTech <cthang94@calvietech.com>
2025-07-30 11:57:44 -0400

    removed secret

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBx20vd1HL862FpWhLFvqmtAkknYe_saUo",
-  authDomain: "yeetflex-1d350.firebaseapp.com",
-  projectId: "yeetflex-1d350",
-  storageBucket: "yeetflex-1d350.appspot.com",
-  messagingSenderId: "613080385673",
-  appId: "1:613080385673:web:e2c597475ca7bbf5085e58",
-  measurementId: "G-8TXRNRL3ZR",
-};
+
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
