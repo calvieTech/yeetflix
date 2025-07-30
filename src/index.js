a4863bc2a3e4023878ca7f02770c7aafa982d749
calvieTech <cthang94@calvietech.com>
2025-07-30 11:48:12 -0400

    created Firebase

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "../src/sass/index.scss";
-import App from "./App";
-import store from "./app/store";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../src/sass/index.scss';
+import App from './App';
+import store from './app/store';
+import { Provider } from 'react-redux';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <App />
